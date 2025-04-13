e159c73277e66e20aa820f22eaca4af56ac37d84
ha <withme1004v@naver.com>
2025-04-13 11:08:37 +0900

    first commit

--- app/layout/todoLayout.tsx
+++ app/layout/todoLayout.tsx
@@ -1,10 +1,15 @@
 import { Link, Outlet } from 'react-router'
+import { useCountStore } from '~/store/useCountStore'
 
 export default function TodoLayout() {
+
+const {count} = useCountStore()
+
     return (
         <div className="min-h-screen flex flex-col">
             <header className="bg-blue-600 text-white p-4 shadow-md">
                 <h1 className="text-2xl font-bold">Todo App</h1>
+                <div className='text-2xl text-red-400'>{count} </div>
                 <nav className="mt-2">
                     <ul className="flex gap-4">
                         <li>
--- app/routes/todo/addPage.tsx
+++ app/routes/todo/addPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import AddComponent from "~/components/todo/addComponent";
 
 function AddPage() {
     return (
         <div>
             <div className={'text-4xl'}>Add Page</div>
+
+            <AddComponent></AddComponent>
         </div>
     );
 }
